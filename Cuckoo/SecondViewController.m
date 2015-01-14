531c8d3f9fba55ee139468032bdd850a19c206a3
Lucien Wei <lucienwei@Lucien-MacBook.local>
2015-01-14 16:16:03 +0800

    继续测试git的使用...

--- Cuckoo/SecondViewController.m
+++ Cuckoo/SecondViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"%@", @"测试");
+    NSLog(@"%@", @"继续测试");
 }
 
 - (void)didReceiveMemoryWarning {
